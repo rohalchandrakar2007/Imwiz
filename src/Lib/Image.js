69fb9b592541484170b0e44e73ac294168694a51
Rohal Chandrakar <rohalchandrakar5@gmail.com>
2015-12-23 12:22:53 +0530

    Updated js class standars

--- src/Lib/Image.js
+++ src/Lib/Image.js
@@ -1,50 +1,59 @@
+IMWIZ.Image = function (__data) {
+    this.imageData = new Array();
+    this.width = 0;
+    this.height = 0;
 
-
-
-IMWIZ.Image = function(__data)
-{
-    this.imageData = __data.data;
+    Object.assign(this.imageData, __data.data);
     this.width = __data.width;
     this.height = __data.height;
-    //console.log(__data);
+};
+
+
+
+IMWIZ.Image.prototype = Object.create(IMWIZ.Image.prototype);
+IMWIZ.Image.prototype.constructor = IMWIZ.Image;
+
+
+
+IMWIZ.Image.prototype.getPixel = function (__x, __y) {
+    var pix = new Array();
+    var origin = (__y * this.width + __x) * 4;
+    pix.push(this.imageData[origin]);
+    pix.push(this.imageData[origin + 1]);
+    pix.push(this.imageData[origin + 2]);
+    pix.push(this.imageData[origin + 3]);
+
+    return pix;
+};
+
+
+
+IMWIZ.Image.prototype.setPixel = function (__x, __y, __r, __g, __b, __a) {
+    var origin = (__y * this.width + __x) * 4;
+    this.imageData[origin] = __r;
+    this.imageData[origin + 1] = __g;
+    this.imageData[origin + 2] = __b;
+    this.imageData[origin + 3] = __a;
+};
+
+
+
+IMWIZ.Image.prototype.getImage = function () {
+    var canvas = document.createElement('canvas');
+    this.img = canvas.getContext('2d').createImageData(this.width, this.height);
+    this.img.data.set(this.imageData);
+
+    return this.img;
 };
 
 
 
 
-
-
-IMWIZ.Image.prototype.getPixel = function (__x, __y)
-{
-    var pix = new Array();
-    var origin = (__y*this.width + __x) * 4;
-    pix.push(this.imageData[origin]);
-    pix.push(this.imageData[origin+1]);
-    pix.push(this.imageData[origin+2]);
-    pix.push(this.imageData[origin+3]);
-    
-    return pix;
-}
-
-
-
-IMWIZ.Image.prototype.setPixel = function (__x, __y, __r, __g, __b, __a)
-{
-    var origin = (__y*this.width + __x) * 4;
-    this.imageData[origin] = __r;
-    this.imageData[origin+1] = __g;
-    this.imageData[origin+2] = __b;
-    this.imageData[origin+3] = __a;
-}
-
-
-
-
-
-
-
-IMWIZ.Image.prototype.getImageData = function()
-{
+IMWIZ.Image.prototype.getImageData = function () {
     return this.imagedata;
 };
 
+
+IMWIZ.Image.prototype.clone = function () {
+    return new this.constructor().copy(this);
+};
--- src/test.js
+++ src/test.js
@@ -1,75 +1,77 @@
 (function () {
-    var imwiObj;
+        var imwiObj;
 
-    function init() {
-        console.log("init...");
+        function init() {
+            console.log("init...");
 
-        this.image = new Image();
-        this.image.src = 'temptextures/small_building.png';
-        this.image.onload = _imageLoaded;
-    }
-
-
-
-
-
-
-    function _imageLoaded(ev) {
-        element = document.createElement("canvas");
-        c = element.getContext("2d");
-        im = ev.target;
-        width = im.width;
-        height = im.height;
-        c.drawImage(im, 0, 0);
-        imData = c.getImageData(0, 0, width, height);
-        
-        //code for changing pixepls
-        imwiObj = new IMWIZ.Image(imData);
-        
-        console.log(imwiObj);
-        
-        var tObj = goclone(imwiObj);
-        tObj.setPixel(0,0,200,0,0,0);
-        
-        console.log(tObj);
-        
-        _drawImage(imData);
-        
-        
-    }
-    
-    
-    
-    function goclone(source) {
-    if (Object.prototype.toString.call(source) === '[object Array]') {
-        var clone = [];
-        for (var i=0; i<source.length; i++) {
-            clone[i] = goclone(source[i]);
+            this.image = new Image();
+            this.image.src = 'temptextures/small_building.png';
+            this.image.onload = _imageLoaded;
         }
-        return clone;
-    } else if (typeof(source)=="object") {
-        var clone = {};
-        for (var prop in source) {
-            if (source.hasOwnProperty(prop)) {
-                clone[prop] = goclone(source[prop]);
-            }
+
+
+
+
+
+
+        function _imageLoaded(ev) {
+            element = document.createElement("canvas");
+            c = element.getContext("2d");
+            im = ev.target;
+            width = im.width;
+            height = im.height;
+            c.drawImage(im, 0, 0);
+            imData = c.getImageData(0, 0, width, height);
+            
+             
+            //code for changing pixepls
+            imwiObj = new IMWIZ.Image(imData);
+            
+           
+            
+
+            console.log(imwiObj);
+
+            var tObj = new IMWIZ.Image(imData);
+            tObj.setPixel(0,0,200,0,0,255);
+            tObj.setPixel(1,0,200,0,0,255);
+            tObj.setPixel(2,0,200,0,0,255);
+            tObj.setPixel(3,0,200,0,0,255);
+            tObj.setPixel(4,0,200,0,0,255);
+            tObj.setPixel(5,0,200,0,0,255);
+            tObj.setPixel(6,0,200,0,0,255);
+            tObj.setPixel(7,0,200,0,0,255);
+            tObj.setPixel(8,0,200,0,0,255);
+            tObj.setPixel(9,0,200,0,0,255);
+
+            console.log(tObj);
+
+            _drawImage(tObj.getImage());
+
         }
-        return clone;
-    } else {
-        return source;
-    }
-}
 
 
+
+        
     
-    
-    
-    
-    
-    
-    
-    function _drawImage(__imageData)
-    {
+
+
+    var clone = (function () {
+            return function (obj) {
+                Clone.prototype = obj;
+                return new Clone()
+            };
+
+            function Clone() {}
+        }());
+
+
+
+
+
+
+
+    function _drawImage(__imageData) {
         element = document.getElementById("myCanvas");
         c = element.getContext("2d");
         c.putImageData(__imageData, 0, 0);
